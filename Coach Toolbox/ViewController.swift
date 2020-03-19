8110987b8644e3797731f8ef4ca833bec0e43d58
Eric Romrell <eric_romrell@byu.edu>
2020-03-18 20:41:25 -0600

    Added new project

---
+++ Coach Toolbox/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Coach Toolbox
+//
+//  Created by Eric Romrell on 3/18/20.
+//  Copyright © 2020 Eric Romrell. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
